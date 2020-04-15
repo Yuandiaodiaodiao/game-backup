6b138995310eff7351c00deaf6eafc9ee230c5d0
苑 <33371662+Yuandiaodiaodiao@users.noreply.github.com>
2020-04-15 17:13:38 +0800

    添加骑砍自动备份

---
+++ mb_backup.py
@@ -0,0 +1,33 @@
+from watchdog.events import FileSystemEventHandler,LoggingEventHandler
+from watchdog.observers import Observer
+import time
+import os
+import shutil
+FILENAME='save_auto.sav'
+class FileHandler(FileSystemEventHandler):
+
+    def on_modified(self, event):
+        try:
+            _,filename=os.path.split(event.src_path)
+            if filename==FILENAME:
+                print(f"文件被修改了  {filename}")
+                shutil.copy(src=FILENAME,dst=f"AUTOBACK{time.strftime('%m-%d %H %M %S',time.localtime(time.time()))}.sav")
+                files=list(filter(lambda x: "AUTOBACK" in x, os.listdir('.')))
+                files_with_time=list(map(lambda x:(x,os.path.getmtime(x)),files))
+                files_with_time.sort(key=lambda x: 1-x[1])
+                files_to_delete=files_with_time[20:]
+                # print(files_to_delete)
+                for file in files_to_delete:
+                    print(f"清理旧存档{file}")
+                    os.remove(file[0])
+        except Exception as e:
+            print(e)
+            pass
+
+event_handler = FileHandler()
+observer = Observer()
+observer.schedule(event_handler, ".")
+observer.start()
+while True:
+    time.sleep(1)
+
--- test.py
+++ test.py
@@ -2,11 +2,13 @@
 from win32file import CreateFile, SetFileTime, CloseHandle,GENERIC_READ, GENERIC_WRITE, OPEN_EXISTING
 from pywintypes import Time
 import time
-fh = CreateFile("pythonbackup0.zip", GENERIC_READ | GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, 0)
-
-timex=time.localtime(time.time())
-time2=time.mktime(timex)
-createTimes = Time(time2)
-print(createTimes)
-SetFileTime(fh,createTimes,createTimes,createTimes)
-CloseHandle(fh)
+# fh = CreateFile("pythonbackup0.zip", GENERIC_READ | GENERIC_WRITE, 0, None, OPEN_EXISTING, 0, 0)
+#
+# timex=time.localtime(time.time())
+# time2=time.mktime(timex)
+# createTimes = Time(time2)
+# print(createTimes)
+# SetFileTime(fh,createTimes,createTimes,createTimes)
+# CloseHandle(fh)
+import time
+print(time.strftime('%m-%d %H:%M:%S',time.localtime(time.time())))
